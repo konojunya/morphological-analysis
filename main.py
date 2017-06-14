8f188b3b9d977de44cad8d8893f285b7a8d57d08
konojunya <junya03dance@gmail.com>
2017-06-14 14:58:28 +0900

    関数化 and pep8に準ずるコードへ変換

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import MeCab
+import glob
 
-mecab = MeCab.Tagger('-d /usr/local/mecab/lib/mecab/dic/mecab-ipadic-neologd')
 
-text = "あしたハ 〜ハッシュドポテト食べて面接頑張ろウ🤤🖤✌🏿️緊張。"
+def read():
+    dirs = glob.glob("/Users/kounojunya/dev/hobby/watching-tweets/tweets/*")
+    for item in dirs:
+        for path in glob.glob(item + "/*.txt"):
+            for f_text in open(path, "r"):
+                analysis(f_text)
 
-mecab.parse("") # 文字列がGCされるのを防ぐ
-node = mecab.parseToNode(text)
-while node:
-    # 単語を取得
-    word = node.surface
-    #品詞を取得
-    pos = node.feature.split(",")[1]
-    print('{0} , {1}'.format(word,pos))
-    #次の単語にすすめる
-    node = node.next
+
+def analysis(text):
+    node = mecab.parseToNode(text)
+    while node:
+        # 単語を取得
+        word = node.surface
+        # 品詞を取得
+        pos = node.feature.split(",")[1]
+        print('{0} , {1}'.format(word, pos))
+        # 次の単語にすすめる
+        node = node.next
+
+
+def start():
+    read()
+
+
+if __name__ == "__main__":
+    mecab = MeCab.Tagger(
+        '-d /usr/local/mecab/lib/mecab/dic/mecab-ipadic-neologd')
+    mecab.parse("")  # 文字列がGCされるのを防ぐ
+
+    start()
